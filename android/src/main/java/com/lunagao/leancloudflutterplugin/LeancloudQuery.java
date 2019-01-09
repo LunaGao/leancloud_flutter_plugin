4c07336ad06cb3ab9f62e94de68508c4abb4e75a
Luna <gao.xue.qi@qq.com>
2019-01-10 00:19:00 +0800

    add android query

--- android/src/main/java/com/lunagao/leancloudflutterplugin/LeancloudQuery.java
+++ android/src/main/java/com/lunagao/leancloudflutterplugin/LeancloudQuery.java
@@ -1,6 +1,7 @@
 package com.lunagao.leancloudflutterplugin;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 import java.lang.reflect.InvocationTargetException;
@@ -10,6 +11,8 @@ import cn.leancloud.AVObject;
 import cn.leancloud.AVQuery;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 class LeancloudQuery {
 
@@ -34,33 +37,87 @@ class LeancloudQuery {
      * @param call MethodCall from LeancloudFlutterPlugin.onMethodCall function
      * @param result MethodChannel.Result from LeancloudFlutterPlugin.onMethodCall function
      */
-    void query(MethodCall call, MethodChannel.Result result) {
+    void query(MethodCall call, final MethodChannel.Result result) {
         JSONObject avQueryJson = LeancloudArgsConverter.getAVQueryJsonObject(call, result);
         assert avQueryJson != null;
         String className = avQueryJson.getString("className");
-        String fieldsString = avQueryJson.getString("queries");
-        JSONObject fieldsJson = JSON.parseObject(fieldsString);
+        String queriesString = avQueryJson.getString("queries");
+        JSONArray queriesJson = JSON.parseArray(queriesString);
         AVQuery<AVObject> avQuery = new AVQuery<>(className);
-        try {
-            if (fieldsJson.size() == 1 && fieldsJson.containsKey("get")) {
-//                AVObject object = avQuery.get(fieldsJson.getString("get"));
-                List<AVObject> objects = avQuery.find();
-                AVObject object = avQuery.get("5c31b14544d904005d1e773c");
-                result.success(object);
-//                result.success("Android " + android.os.Build.VERSION.RELEASE);
+        for (Object oneQueryObject : queriesJson) {
+            JSONObject oneQuery;
+            if (oneQueryObject instanceof JSONObject) {
+                oneQuery = (JSONObject) oneQueryObject;
+            } else {
+                oneQuery = (JSONObject) JSON.toJSON(oneQueryObject);
             }
 
-            //TODO list need to be JSON type
-//            result.success(list);
-        } catch (Exception ex) {
-            result.error("aaa" + ex.getMessage(), null, null);
+            switch (oneQuery.getString("queryMethod")) {
+                case "get":
+                    avQuery.getInBackground(oneQuery.getString("arg1")).subscribe(new Observer<AVObject>() {
+                        @Override
+                        public void onSubscribe(Disposable disposable) {}
+
+                        @Override
+                        public void onNext(AVObject avObject) {
+                            result.success(avObject.toJSONObject().toJSONString());
+                        }
+
+                        @Override
+                        public void onError(Throwable throwable) {
+                            result.error("leancloud-error", throwable.getMessage(), null);
+                        }
+
+                        @Override
+                        public void onComplete() {}
+                    });
+                    return;
+                case "equalTo":
+                    avQuery.whereEqualTo(oneQuery.getString("arg1"), oneQuery.get("arg2"));
+                    break;
+                case "notEqualTo":
+                    avQuery.whereNotEqualTo(oneQuery.getString("arg1"), oneQuery.get("arg2"));
+                    break;
+                case "greaterThan":
+                    avQuery.whereGreaterThan(oneQuery.getString("arg1"), oneQuery.get("arg2"));
+                    break;
+                case "greaterThanOrEqualTo":
+                    avQuery.whereGreaterThanOrEqualTo(oneQuery.getString("arg1"), oneQuery.get("arg2"));
+                    break;
+                case "lessThan":
+                    avQuery.whereLessThan(oneQuery.getString("arg1"), oneQuery.get("arg2"));
+                    break;
+                case "lessThanOrEqualTo":
+                    avQuery.whereLessThanOrEqualTo(oneQuery.getString("arg1"), oneQuery.get("arg2"));
+                    break;
+                default:
+                    result.error("params-error", "no such query queryMethod name, please check again", null);
+                    break;
+            }
+
         }
-//        } catch (NoSuchMethodException ex) {
-//            result.error("NoSuchMethodException, please check queryMethod", null, null);
-//        } catch (IllegalAccessException ex) {
-//            result.error("IllegalAccessException, Do you call an illegal access method?", null, null);
-//        } catch (InvocationTargetException ex) {
-//            result.error("InvocationTargetException, I don't know what's happen,:( check everything again?", null, null);
-//        }
+        avQuery.findInBackground().subscribe(new Observer<List<AVObject>>() {
+            @Override
+            public void onSubscribe(Disposable disposable) {}
+
+            @Override
+            public void onNext(List<AVObject> avObjects) {
+                JSONObject jsonObject = new JSONObject();
+                JSONArray jsonArray = new JSONArray();
+                for (AVObject object : avObjects) {
+                    jsonArray.add(object.toJSONObject().toJSONString());
+                }
+                jsonObject.put("objects", jsonArray);
+                result.success(jsonObject.toJSONString());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                result.error("leancloud-error", throwable.getMessage(), null);
+            }
+
+            @Override
+            public void onComplete() {}
+        });
     }
 }
