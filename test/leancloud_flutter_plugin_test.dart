72df3b1550e79ee591fdb39adcd930bac68384e7
Luna Gao <luna.gao@west.com>
2019-09-11 09:41:14 +0800

    update travis file

--- test/leancloud_flutter_plugin_test.dart
+++ test/leancloud_flutter_plugin_test.dart
@@ -4,8 +4,8 @@ import 'package:leancloud_flutter_plugin/leancloud_flutter_plugin.dart';
 void main() {
   test('my first unit test', () {
     var lfp = LeancloudFlutterPlugin.getInstance();
-    lfp.setServer(LeancloudOSService.API, "https://api.maomishen.com");
-    lfp.initialize("", "");
+    lfp.setServer(LeancloudOSService.API, "#LeancloudOSService#");
+    lfp.initialize("#appId#", "#appKey#");
     AVObject object = new AVObject("DemoObject");
     object.put("description", "maomishen!");
     object.put("value", "int->10, boolean->true, float->10.01, ");
